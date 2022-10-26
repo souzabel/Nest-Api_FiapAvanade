e86a7f9ed10359ec98fd6545d30fe3eab5176894
Isabel Pereira de Souza <contatobelsouza@gmail.com>
2022-10-26 09:49:47 -0300

    Deploy

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, ParseUUIDPipe, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDTO } from './dto/createUser.dto';
 
@@ -12,8 +12,16 @@ export class UsersController {
   create(@Body() req: CreateUserDTO) {
     return this.usersService.create();
   }
-  // listar todos
-  // listar um
+  // listar todos localhost:3000/users
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+  // listar um localost:3000/users/1
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: number) {
+    return this.usersService.findOne(id);
+  }
   // atualizar
   // deletar
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDTO } from './dto/createUser.dto';
 
 @Injectable()
 export class UsersService {
   async create(): Promise<string> {
     return 'Usuário criado com sucesso!';
+  }
+  async findAll(): Promise<string> {
+    return 'Lista de usuários!';
+  }
+
+  async findOne(id: number): Promise<string> {
+    return `Usuário ${id}!`;
   }
 }
